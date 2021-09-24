5aa53f863416d9e01a353d9c2e710edde6031f26
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-09-24 21:04:48 +0200

    Tidy StartUp class

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,15 +1,11 @@
+using API.Extensions;
 using Application.BlogPosts;
-using Application.Core;
 using FluentValidation.AspNetCore;
-using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using Persistence;
 
 namespace API
 {
@@ -29,14 +25,7 @@ namespace API
             {
                 config.RegisterValidatorsFromAssemblyContaining<Create>();
             });
-            services.AddDbContext<DataContext>(options =>
-                options.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
-            services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddApplicationServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
