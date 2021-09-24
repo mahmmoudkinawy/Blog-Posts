728f8fc458a84cb4d5c6a4878b54adedef16b994
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2021-09-24 19:42:09 +0200

    Added Create and Delete end points

--- API/Controllers/BlogPostsController.cs
+++ API/Controllers/BlogPostsController.cs
@@ -1,6 +1,5 @@
 ﻿using Application.BlogPosts;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,26 +9,30 @@ namespace API.Controllers
 {
     public class BlogPostsController : BaseApiController
     {
-        private readonly IMediator _mediator;
-
-        public BlogPostsController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
 
         [HttpGet]
         public async Task<ActionResult<List<BlogPost>>> GetBlogPosts()
         {
-            return Ok(await _mediator.Send(new List.Query()));
+            return Ok(await Mediator.Send(new List.Query()));
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<BlogPost>> GetBlogPost(Guid id)
         {
-            return Ok(await _mediator.Send(new Details.Query { Id = id }));
+            return Ok(await Mediator.Send(new Details.Query { Id = id }));
         }
 
+        [HttpPost]
+        public async Task<IActionResult> CreateBlogPost(BlogPost blogPost)
+        {
+            return Ok(await Mediator.Send(new Create.Command { BlogPost = blogPost }));
+        }
 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBlogPost(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command { Id = id }));
+        }
 
     }
 }
